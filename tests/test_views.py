d171d0bbd5bd201663ad21c97f976b1fda0bd8df
gr1d99 <gideonkimutai9@gmail.com>
2017-09-20 16:07:16 +0300

    Heroku setup

--- tests/test_views.py
+++ tests/test_views.py
@@ -9,39 +9,6 @@ class ShoppingAppTest(TestCase):
         app.config['TESTING'] = True
         return app
 
-    # ----------TemplatesTest------------- #
-
-    def test_index_template(self):
-        """
-        test whether the index.html template is the one used
-        :return: True, False
-        """
-        app.test_client().get('/')
-        self.assert_template_used('index.html')
-
-    def test_login_template(self):
-        """
-        test whether the login.html template is the one used
-        :return: True, False
-        """
-        app.test_client().get('/login')
-        self.assert_template_used('login.html')
-
-    def test_dashboard_template(self):
-        """
-        test whether the index.html template is the one used
-        :return: True, False
-        """
-        app.test_client().get('/dashboard')
-        self.assert_template_used('dashboard.html')
-
-    def test_register_template(self):
-        """
-        test whether the register.html template is the one used
-        :return: True, False
-        """
-        app.test_client().get('/register')
-        self.assert_template_used('dashboard.html')
-
-
-
+    def test_login(self):
+        resp = app.test_client.post('/login', data=dict(username='admin', password='user'))
+        self.assertRedirects(resp.status_code, location='/')
