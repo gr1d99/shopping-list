3aedff610591874c9b2373109c3d5e7588304bf0
gr1d99 <gideonkimutai9@gmail.com>
2017-09-20 06:28:30 +0300

    Add tests folder

---
+++ tests/views_test.py
@@ -0,0 +1,23 @@
+import unittest
+from app.shopping_app import app
+
+
+class ViewsTest(unittest.TestCase):
+    """
+    This testcase will is a passing test and it will test if the status
+    code of the response returned is 200 OK
+    """
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_views(self):
+        index = self.app.get('/')
+        dashboard = self.app.get('/dashboard')
+        login = self.app.get('/login')
+        register = self.app.get('/register')
+
+        self.assertEqual(index.status_code, 200)
+        self.assertEqual(dashboard.status_code, 200)
+        self.assertEqual(login.status_code, 200)
+        self.assertEqual(register.status_code, 200)
