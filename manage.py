5fbbe3e48b7e76f86237eba71f283f23d7787d0e
gr1d99 <gideonkimutai9@gmail.com>
2017-09-22 06:07:02 +0300

    Remove unused variables

--- manage.py
+++ manage.py
@@ -1,4 +1,3 @@
-from shopping_app.db.models import SHOPPING_FILE, USERS_FILE
 import json
 from flask_script import Manager
 from main import app
@@ -10,14 +9,6 @@ manager = Manager(app)
 @manager.command
 def generate_secret():
     secret_key_gen()
-
-
-@manager.command
-def resetdb():
-    files = [SHOPPING_FILE, USERS_FILE]
-    for file in files:
-        with open(file, 'w') as f:
-            json.dump([], f)
 
 
 if __name__ == "__main__":
