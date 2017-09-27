a49dd367f5fc50f27780be63be942430e6258a2f
gr1d99 <gideonkimutai9@gmail.com>
2017-09-27 11:52:31 +0300

    Add mark item view

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 from shopping_app.db.models import User, ShoppingItem
-from shopping_app.views import (IndexView, AddItemsView, CreateShoppingList, DashboardView,
+from shopping_app.views import (IndexView, AddItemsView, CreateShoppingList, DashboardView, MarkItemView,
                                 LoginView, Logout, RegisterView, RemoveShoppingList, NewShoppingListDetailView)
 
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
@@ -20,6 +20,7 @@ app.add_url_rule('/login', view_func=LoginView.as_view('login'))
 app.add_url_rule('/logout', view_func=Logout.as_view('logout'))
 app.add_url_rule('/register', view_func=RegisterView.as_view('register'))
 app.add_url_rule('/create-shopping-list', view_func=CreateShoppingList.as_view('create-shopping-list'))
+app.add_url_rule('/mark-item', view_func=MarkItemView.as_view('mark-item'))
 app.add_url_rule('/shopping-list-detail/', view_func=NewShoppingListDetailView.as_view('shopping-list-detail'))
 app.add_url_rule('/remove-shopping-list', view_func=RemoveShoppingList.as_view('remove-shopping-list'))
 
