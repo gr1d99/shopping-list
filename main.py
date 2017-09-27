fb56e7ebe9232055da22d125b92ab4e4cee3b204
gr1d99 <gideonkimutai9@gmail.com>
2017-09-27 05:01:26 +0300

    Update Shopping List manipulation views

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 from flask import Flask
-from shopping_app.db.models import User, ShoppingList
+from shopping_app.db.models import User, ShoppingItem
 from shopping_app.views import (IndexView, AddItemsView, CreateShoppingList, DashboardView,
-                                LoginView, Logout, RegisterView, RemoveShoppingList, ShoppingListDetail)
+                                LoginView, Logout, RegisterView, RemoveShoppingList, NewShoppingListDetailView)
 
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -10,7 +10,8 @@ app = Flask(__name__)
 app.template_folder = os.path.join(BASEDIR, 'shopping_app/templates')
 app.static_folder = os.path.join(BASEDIR, 'shopping_app/static')
 app.user = User()  # user instance
-app.shopping_list = ShoppingList()  # shopping list instance
+app.shopping_item = ShoppingItem  # initialize shopping item
+app.shopping_list = []  # create an empty list that will hold shopping list instances
 
 # app urls
 app.add_url_rule('/', view_func=IndexView.as_view('index'))
@@ -19,7 +20,7 @@ app.add_url_rule('/login', view_func=LoginView.as_view('login'))
 app.add_url_rule('/logout', view_func=Logout.as_view('logout'))
 app.add_url_rule('/register', view_func=RegisterView.as_view('register'))
 app.add_url_rule('/create-shopping-list', view_func=CreateShoppingList.as_view('create-shopping-list'))
-app.add_url_rule('/shopping-list-detail/', view_func=ShoppingListDetail.as_view('shopping-list-detail'))
+app.add_url_rule('/shopping-list-detail/', view_func=NewShoppingListDetailView.as_view('shopping-list-detail'))
 app.add_url_rule('/remove-shopping-list', view_func=RemoveShoppingList.as_view('remove-shopping-list'))
 
 # app conf
