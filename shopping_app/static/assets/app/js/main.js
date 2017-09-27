c4b738dba1f8a378550f2811cadfc7acb7bdaf2d
gr1d99 <gideonkimutai9@gmail.com>
2017-09-27 11:56:36 +0300

    Update dynamic css styling

--- shopping_app/static/assets/app/js/main.js
+++ shopping_app/static/assets/app/js/main.js
@@ -7,6 +7,10 @@ $(document).ready(function () {
     $("#shl-item, #shl-price").addClass('text-center');
     $(".shl-input").addClass('text-center');
     $("#username, #password").addClass('form-control');
+    $('#item_name').addClass('form-control').attr('placeholder', 'Item name');
+    $('#quantity').addClass('form-control').attr('placeholder', 'Quantity');
+    $('#price').addClass('form-control').attr('placeholder', 'Price per quantity');
+    $('li.form-error').addClass('text-danger');
 });
 
 document.getElementById('username').placeholder = 'Username';
