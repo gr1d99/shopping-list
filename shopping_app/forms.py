442d827e46533401302ddff1144ee02b0c25ec13
gr1d99 <gideonkimutai9@gmail.com>
2017-09-29 12:26:02 +0300

    Fix: Add bad punctuation characters validation check

--- shopping_app/forms.py
+++ shopping_app/forms.py
@@ -1,14 +1,28 @@
 """Contains all needed forms"""
 
+import string
 from wtforms import (Form, DecimalField, IntegerField, StringField,
-                     PasswordField, validators, BooleanField)
+                     PasswordField, validators, BooleanField, ValidationError)
 from wtforms.validators import DataRequired, InputRequired
+
+BAD_CHARS = (c for c in string.punctuation)  # bad characters generator
 
 
 class LoginForm(Form):
     """For to handle login"""
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
+
+    def validate_name(self, field):
+        """
+        Validate punctuation characters
+        :param field:
+        :return:
+        """
+        name = [n for n in field.data]
+        for c in BAD_CHARS:
+            if c in name:
+                raise ValidationError("%(c)s is an invalid name" % dict(c=c))
 
 
 class CreateShoppingItemForm(Form):
@@ -17,6 +31,17 @@ class CreateShoppingItemForm(Form):
     quantity = IntegerField('quantity', validators=[InputRequired()])
     price = DecimalField('price', validators=[InputRequired()])
     checked = BooleanField('checked')
+
+    def validate_item_name(self, field):
+        """
+        Validate punctuation characters
+        :param field:
+        :return:
+        """
+        name = [n for n in field.data]
+        for c in BAD_CHARS:
+            if c in name:
+                raise ValidationError("%(c)s is an invalid name" % dict(c=c))
 
 
 class RegistrationForm(Form):
@@ -30,7 +55,29 @@ class RegistrationForm(Form):
     ])
     confirm = PasswordField('Repeat Password')
 
+    def validate_username(self, field):
+        """
+        Validate punctuation characters
+        :param field:
+        :return:
+        """
+        name = [n for n in field.data]
+        for c in BAD_CHARS:
+            if c in name:
+                raise ValidationError("%(c)s is an invalid name" % dict(c=c))
+
 
 class CreateShoppingListForm(Form):
     """A form to handle creation of shopping list"""
     name = StringField('name', validators=[InputRequired()])
+
+    def validate_name(self, field):
+        """
+        Validate bad characters
+        :param field:
+        :return:
+        """
+        name = [n for n in field.data]
+        for c in BAD_CHARS:
+            if c in name:
+                raise ValidationError("%(c)s is an invalid name" % dict(c=c))
