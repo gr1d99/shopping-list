e0e991a7245b6447efb6dd7f5d389957817e66a5
gr1d99 <gideonkimutai9@gmail.com>
2017-09-30 08:47:34 +0300

    [Fix]Remove redundant validator

--- shopping_app/forms.py
+++ shopping_app/forms.py
@@ -1,52 +1,28 @@
 """Contains all needed forms"""
 
-import string
 from wtforms import (Form, DecimalField, IntegerField, StringField,
                      PasswordField, validators, BooleanField, ValidationError)
 from wtforms.validators import DataRequired, InputRequired
-
-BAD_CHARS = (c for c in string.punctuation)  # bad characters generator
+from .core.validators import validate_names
 
 
 class LoginForm(Form):
     """For to handle login"""
-    username = StringField('username', validators=[DataRequired()])
+    username = StringField('username', validators=[DataRequired(), validate_names])
     password = PasswordField('password', validators=[DataRequired()])
-
-    def validate_name(self, field):
-        """
-        Validate punctuation characters
-        :param field:
-        :return:
-        """
-        name = [n for n in field.data]
-        for c in BAD_CHARS:
-            if c in name:
-                raise ValidationError("%(c)s is an invalid name" % dict(c=c))
 
 
 class CreateShoppingItemForm(Form):
     """A form to handle creation of shopping items"""
-    item_name = StringField('item-name', validators=[InputRequired()])
+    item_name = StringField('item-name', validators=[validate_names, InputRequired()])
     quantity = IntegerField('quantity', validators=[InputRequired()])
     price = DecimalField('price', validators=[InputRequired()])
     checked = BooleanField('checked')
 
-    def validate_item_name(self, field):
-        """
-        Validate punctuation characters
-        :param field:
-        :return:
-        """
-        name = [n for n in field.data]
-        for c in BAD_CHARS:
-            if c in name:
-                raise ValidationError("%(c)s is an invalid name" % dict(c=c))
-
 
 class RegistrationForm(Form):
     """A form to handle registration of users"""
-    username = StringField('Username', [validators.Length(min=3, max=25)])
+    username = StringField('Username', [validators.Length(min=3, max=25), validate_names])
     email = StringField('Email Address', [validators.Email(), validators.Length(min=6, max=35)])
     password = PasswordField('New Password', [
         validators.DataRequired(),
@@ -55,29 +31,7 @@ class RegistrationForm(Form):
     ])
     confirm = PasswordField('Repeat Password')
 
-    def validate_username(self, field):
-        """
-        Validate punctuation characters
-        :param field:
-        :return:
-        """
-        name = [n for n in field.data]
-        for c in BAD_CHARS:
-            if c in name:
-                raise ValidationError("%(c)s is an invalid name" % dict(c=c))
-
 
 class CreateShoppingListForm(Form):
     """A form to handle creation of shopping list"""
-    name = StringField('name', validators=[InputRequired()])
-
-    def validate_name(self, field):
-        """
-        Validate bad characters
-        :param field:
-        :return:
-        """
-        name = [n for n in field.data]
-        for c in BAD_CHARS:
-            if c in name:
-                raise ValidationError("%(c)s is an invalid name" % dict(c=c))
+    name = StringField('name', validators=[InputRequired(), validate_names])
