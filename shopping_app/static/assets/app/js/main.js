0593de8acc85318fe6ed58d1639e6092656e04e2
gr1d99 <gideonkimutai9@gmail.com>
2017-10-02 13:32:31 +0300

    Update app javascript file

--- shopping_app/static/assets/app/js/main.js
+++ shopping_app/static/assets/app/js/main.js
@@ -3,21 +3,19 @@
  */
 'use strict';
 
-$(document).ready(function () {
-    $("#shl-item, #shl-price").addClass('text-center');
-    $(".shl-input").addClass('text-center');
-    $("#username, #password, #email, #password, #confirm, #name").addClass('form-control');
-    $('#item_name').addClass('form-control').attr('placeholder', 'Item name');
-    $('#quantity').addClass('form-control').attr('placeholder', 'Quantity');
-    $('#price').addClass('form-control').attr('placeholder', 'Price per quantity');
-    $('#email').addClass('form-control').attr('placeholder', 'Email');
-    $('#confirm').addClass('form-control').attr('placeholder', 'Confirm Password');
-    $('#name').addClass('form-control').attr('placeholder', 'Shopping list name');
-    $('form ul').addClass('list-unstyled');
-    $('form ul li').addClass('form-error');
-    $('li.form-error').addClass('text-danger');
-    $('.ui.dropdown').dropdown();
-});
+$("#shl-item, #shl-price").addClass('text-center');
+$(".shl-input").addClass('text-center');
+$("#username, #password, #email, #password, #confirm, #name").addClass('form-control');
+$('#item_name').addClass('form-control').attr('placeholder', 'Item name');
+$('#quantity').addClass('form-control').attr('placeholder', 'Quantity');
+$('#price').addClass('form-control').attr('placeholder', 'Price per quantity');
+$('#email').addClass('form-control').attr('placeholder', 'Email');
+$('#confirm').addClass('form-control').attr('placeholder', 'Confirm Password');
+$('#name').addClass('form-control').attr('placeholder', 'Shopping list name');
+$('form ul').addClass('list-unstyled');
+$('form ul li').addClass('form-error');
+$('li.form-error').addClass('text-danger');
+$('.ui.dropdown').dropdown();
 
 
 
