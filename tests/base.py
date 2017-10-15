ed96f23409ec150fb81364be1520cedf7e0c1b49
gr1d99 <gideonkimutai9@gmail.com>
2017-10-15 06:08:35 +0300

    Add docstrings

--- tests/base.py
+++ tests/base.py
@@ -1,9 +1,19 @@
+"""
+    contains base test class for all tests
+"""
+
 from flask_testing import TestCase
 from main import APP
 
 
 class ShoppingListTestBase(TestCase):
+    """
+        Base class for all tests cases
+    """
     def create_app(self):
+        """
+        create flask instance
+        """
         app = APP
         app.testing = True
         app.debug = False
@@ -11,4 +21,7 @@ class ShoppingListTestBase(TestCase):
         return app
 
     def setUp(self):
+        """
+        will be called before each test is run
+        """
         self.app = self.create_app()
