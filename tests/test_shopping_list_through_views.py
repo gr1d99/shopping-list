cf6a097daca04b1abe730e077292b919612feb99
gr1d99 <gideonkimutai9@gmail.com>
2017-11-12 18:51:37 +0300

    [Fixture]Add individual tests for shoppinglist and shoppingitem crud functionality.

--- tests/test_shopping_list_through_views.py
+++ tests/test_shopping_list_through_views.py
@@ -20,70 +20,117 @@ class TestShoppingListThroughViews(ShoppingListTestBase):
                                            confirm=user_cridentials.get('password')),
                     follow_redirects=True)  # register client
         self.client = client
-        self.user_cridentials = user_cridentials  # cridentials to authenticate user
+        self.user_cridentials = user_cridentials  # cridentials to authenticate user.
 
-    def test_shopping_list_crud(self):
+    def test_create_shoppinglist(self):
         """
-            test if user can create and view shopping list
+        User can create shoppinglist object.
         """
+
         name = 'Birthday'  # name of the shopping list
         with self.client as client:
-
             #  login user
-            login_resp = client.post('/login',
-                                     data=dict(username=self.user_cridentials.get('username'),
-                                               password=self.user_cridentials.get('password')))
-
-            self.assertRedirects(login_resp, '/')  # assert if user is redirected to index page
+            client.post('/login',
+                        data=dict(username=self.user_cridentials.get('username'),
+                                  password=self.user_cridentials.get('password')))
 
             # create shopping list
             create_resp = client.post('/create-shopping-list', data=dict(name=name),
                                       follow_redirects=True)
 
+            # assertions.
             self.assert200(create_resp)
-            self.assertIn(bytes(name.encode('ascii')), create_resp.data)
+            self.assertIn(name, create_resp.get_data(as_text=True))
 
-            # view shopping list
+    def test_retrieve_shoppinglist(self):
+        """
+        User can retrieve created shoppinglist object.
+        """
+
+        name = 'Birthday'  # name of the shopping list
+        with self.client as client:
+            #  login user
+            client.post('/login',
+                        data=dict(username=self.user_cridentials.get('username'),
+                                  password=self.user_cridentials.get('password')))
+
+            # create shoppinglist object.
+            client.post('/create-shopping-list', data=dict(name=name),
+                        follow_redirects=True)
+
+            # retrieve shoppinglist object.
             view_resp = client.get('/shopping-list-detail/?name=%(name)s'
-                                   % dict(name=name))  # get shopping list detail
+                                   % dict(name=name))
 
-            self.assert200(view_resp)
-            self.assertIn(bytes(name.encode('ascii')), view_resp.data)
+            # assertions.
+            self.assert200(view_resp)  # 200 if request is successful.
+            self.assertIn(name, view_resp.get_data(as_text=True))
 
-            # update shopping list
+    def test_update_shoppinglist(self):
+        """
+        User should update created shoppinglist.
+        """
+
+        name = 'Birthday'  # name of the shopping list
+        with self.client as client:
+            #  login user
+            client.post('/login',
+                        data=dict(username=self.user_cridentials.get('username'),
+                                  password=self.user_cridentials.get('password')))
+
+            # create shoppinglist object.
+            client.post('/create-shopping-list', data=dict(name=name),
+                        follow_redirects=True)
+
+            # update shoppinglist object.
             new_name = 'Birthday Party'
             update_resp = client.post('/update-shopping-list/?name=%(name)s' % dict(name=name),
                                       data=dict(name=new_name), follow_redirects=True)
 
             self.assert200(update_resp)
-            self.assertIn(bytes(new_name.encode('ascii')), update_resp.data)
+            self.assertIn(new_name, update_resp.get_data(as_text=True))
             self.assertMessageFlashed('Shopping list name changed successfully', 'success')
+
+    def test_delete_shoppinglist(self):
+        """
+        User should delete created shoppinglist.
+        """
+
+        name = 'Birthday'  # name of the shopping list
+        with self.client as client:
+            #  login user
+            client.post('/login',
+                        data=dict(username=self.user_cridentials.get('username'),
+                                  password=self.user_cridentials.get('password')))
+
+            # create shoppinglist object.
+            client.post('/create-shopping-list', data=dict(name=name),
+                        follow_redirects=True)
 
             # remove shopping list
             delete_resp = client.get('/remove-shopping-list/?name=%(name)s'
-                                     % dict(name=new_name),
+                                     % dict(name=name),
                                      follow_redirects=True)  # remove shopping list detail
-            # using updated new name
 
             self.assertMessageFlashed('Success!! Shopping List removed', 'success')
             self.assert200(delete_resp)
 
-    def test_create_shopping_item(self):
+    def test_create_shoppingitem(self):
         """
-            test if shopping item is added to the shopping list
+        Client should add shoppingitem object to shoppinglist object.
         """
 
-        shopping_list_name = 'Birthday'  # name of the shopping list
-        shopping_item_one = dict(item_name='Cup cake',
-                                 quantity='10',
-                                 price='100')  # shopping item one data
-        shopping_item_two = dict(item_name='Juice',
-                                 quantity=2,
-                                 price=200)  # shopping item two data
+        shoppinglist_name = 'Birthday'  # name of the shopping list.
+        shoppingitem_one = dict(item_name='Cup cake',
+                                quantity='10',
+                                price='100')  # shopping item one data.
+        shoppingitem_two = dict(item_name='Juice',
+                                quantity=2,
+                                price=200)  # shopping item two data.
 
         with self.client as client:
 
-            #  login user
+            # login user
             login_resp = client.post('/login',
                                      data=dict(username=self.user_cridentials.get('username'),
                                                password=self.user_cridentials.get('password')))
@@ -91,69 +138,161 @@ class TestShoppingListThroughViews(ShoppingListTestBase):
             self.assertRedirects(login_resp, '/')  # assert if user is redirected to index page
 
             # create shopping list
-            create_shopping_list_resp = client.post('/create-shopping-list',
-                                                    data=dict(name=shopping_list_name),
-                                                    follow_redirects=True)
-
-            self.assert200(create_shopping_list_resp)
-            self.assertIn(bytes(shopping_list_name.encode('ascii')),
-                          create_shopping_list_resp.data)
-            self.assertMessageFlashed('Shopping list created', 'success')
-
-            # create shopping item
-            client.post('/shopping-list-detail/?name=%(name)s' %
-                        dict(name=shopping_list_name), data=shopping_item_one,
-                        follow_redirects=True)
-            client.post('/shopping-list-detail/?name=%(name)s' %
-                        dict(name=shopping_list_name), data=shopping_item_two,
+            client.post('/create-shopping-list',
+                        data=dict(name=shoppinglist_name),
                         follow_redirects=True)
 
-            view_shopping_list_resp = client.get('/shopping-list-detail/?name=%(name)s'
-                                                 % dict(name=shopping_list_name))
-            self.assertIn(bytes(shopping_item_one.
-                                get('item_name').encode('ascii')),
-                          view_shopping_list_resp.data)
-            self.assertIn(bytes(shopping_item_two.
-                                get('item_name').encode('ascii')),
-                          view_shopping_list_resp.data)
+            # create shoppingitems.
+            resp_one = client.post('/shopping-list-detail/?name=%(name)s' %
+                                   dict(name=shoppinglist_name), data=shoppingitem_one,
+                                   follow_redirects=True)
+
+            resp_two = client.post('/shopping-list-detail/?name=%(name)s' %
+                                   dict(name=shoppinglist_name), data=shoppingitem_two,
+                                   follow_redirects=True)
+
+            # assertions.
+            self.assert200(resp_one)
+            self.assert200(resp_two)
             self.assertMessageFlashed('Item successfully added', 'success')
 
-            # update shopping item
-            # update `shopping item one` name
+    def test_retrieve_shoppingitem(self):
+        """
+        User should retrieve created shoppingitem object.
+        """
+
+        shoppinglist_name = 'Birthday'  # name of the shopping list.
+        shoppingitem_one = dict(item_name='Cup cake',
+                                quantity='10',
+                                price='100')  # shopping item one data.
+        shoppingitem_two = dict(item_name='Juice',
+                                quantity=2,
+                                price=200)  # shopping item two data.
+
+        with self.client as client:
+            # login user
+            login_resp = client.post('/login',
+                                     data=dict(username=self.user_cridentials.get('username'),
+                                               password=self.user_cridentials.get('password')))
+
+            self.assertRedirects(login_resp, '/')  # assert if user is redirected to index page
+
+            # create shopping list
+            client.post('/create-shopping-list',
+                        data=dict(name=shoppinglist_name),
+                        follow_redirects=True)
+
+            # create shoppingitems.
+            client.post('/shopping-list-detail/?name=%(name)s' %
+                        dict(name=shoppinglist_name), data=shoppingitem_one,
+                        follow_redirects=True)
+
+            client.post('/shopping-list-detail/?name=%(name)s' %
+                        dict(name=shoppinglist_name), data=shoppingitem_two,
+                        follow_redirects=True)
+
+            # retrieve shoppinglist object.
+            view_resp = client.get('/shopping-list-detail/?name=%(name)s' %
+                                   dict(name=shoppinglist_name))
+
+            self.assert200(view_resp)
+            self.assertIn(shoppingitem_one.get('item_name'),
+                          view_resp.get_data(as_text=True))
+            self.assertIn(shoppingitem_two.get('item_name'),
+                          view_resp.get_data(as_text=True))
+
+    def test_update_shoppingitem(self):
+        """
+        User should update shoppingitem object.
+        """
+
+        shoppinglist_name = 'Birthday'  # name of the shopping list.
+        shoppingitem = dict(item_name='Cup cake',
+                            quantity='10',
+                            price='100')  # shopping item one data.
+
+        with self.client as client:
+
+            # login user
+            login_resp = client.post('/login',
+                                     data=dict(username=self.user_cridentials.get('username'),
+                                               password=self.user_cridentials.get('password')))
+
+            self.assertRedirects(login_resp, '/')  # assert if user is redirected to index page
+
+            # create shopping list
+            client.post('/create-shopping-list',
+                        data=dict(name=shoppinglist_name),
+                        follow_redirects=True)
+
+            # create shoppingitems.
+            client.post('/shopping-list-detail/?name=%(name)s' %
+                        dict(name=shoppinglist_name), data=shoppingitem,
+                        follow_redirects=True)
+
+            # update shoppingitem.
             new_item_name = 'Chicken wings'
 
             update_shopping_item_resp = \
                 client.post('/update-item/?sname=%(sname)s&iname=%(iname)s' %
-                            dict(sname=shopping_list_name,
-                                 iname=shopping_item_one.get('item_name')),
+                            dict(sname=shoppinglist_name,
+                                 iname=shoppingitem.get('item_name')),
                             data=dict(item_name=new_item_name,
-                                      quantity=shopping_item_one.get('quantity'),
-                                      price=shopping_item_one.get('price')),
+                                      quantity=shoppingitem.get('quantity'),
+                                      price=shoppingitem.get('price')),
                             follow_redirects=True)
 
             self.assertMessageFlashed('Item successfully updated', 'success')
-            self.assertIn(bytes(new_item_name.encode('ascii')), update_shopping_item_resp.data)
+            self.assertIn(new_item_name, update_shopping_item_resp.get_data(as_text=True))
 
-            # delete shopping item
-            # delete `shopping_item_two`
+    def test_delete_shoppingitem(self):
+        """
+        User should delete shoppingitem object.
+        """
+
+        shoppinglist_name = 'Birthday'  # name of the shopping list.
+        shoppingitem = dict(item_name='Cup cake',
+                            quantity='10',
+                            price='100')  # shopping item one data.
+
+        with self.client as client:
+
+            # login user
+            login_resp = client.post('/login',
+                                     data=dict(username=self.user_cridentials.get('username'),
+                                               password=self.user_cridentials.get('password')))
+
+            self.assertRedirects(login_resp, '/')  # assert if user is redirected to index page
+
+            # create shopping list
+            client.post('/create-shopping-list',
+                        data=dict(name=shoppinglist_name),
+                        follow_redirects=True)
+
+            # create shoppingitems.
+            client.post('/shopping-list-detail/?name=%(name)s' %
+                        dict(name=shoppinglist_name), data=shoppingitem,
+                        follow_redirects=True)
+
+            # delete shoppingitem.
             del_response = \
                 client.get('/remove-shopping-item/?name=%(sname)s&item_name=%(iname)s' %
-                           dict(sname=shopping_list_name, iname=shopping_item_two.get('item_name')),
+                           dict(sname=shoppinglist_name, iname=shoppingitem.get('item_name')),
                            follow_redirects=True)
 
+            self.assert200(del_response)
             self.assertMessageFlashed('Success!! Item succesfully removed', 'success')
-            self.assertTrue(bytes(shopping_item_two.get('item_name').encode('ascii'))
-                            not in del_response.data)
 
     def test_non_existence(self):
         """
-            tests for non existence shopping lists and shopping items with authenticated user
+        tests for non existing shoppinglists and shoppingitems with authenticated user.
         """
 
         with self.client as client:
             # attempt to delete a shopping list
             del_response = client.get('/remove-shopping-item/?name=HomeComings&item_name=Pizza',
                                       follow_redirects=True)
+
             self.assertMessageFlashed('you must be logged in, '
                                       'or create an account if you dont have one')
 
